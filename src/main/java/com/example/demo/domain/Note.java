3c9040311e5bbcabb794acc0b070915415c26fee
Bartek Dec <decbartek@gmail.com>
2020-11-25 11:54:30 +0100

    added Note entity

---
+++ src/main/java/com/example/demo/domain/Note.java
@@ -0,0 +1,43 @@
+package com.example.demo.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title can not be blank")
+    private String title;
+
+    @NotBlank(message = "Content can not be blank")
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
